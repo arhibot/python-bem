25e7ca231d609a3f8fdde17e7ae0b2f80f43783a
Evgeny V. Generalov <e.generalov@gmail.com>
2013-01-18 23:52:02 +0600

    Workaround for PyV8.JSExtension Unicode issue

--- pybem/pybem.py
+++ pybem/pybem.py
@@ -63,8 +63,7 @@ class BEMRender(object):
                     jsdata.append(open(path).read())
                 except IOError:
                     pass
-        return '\n'.join(jsdata)
-
+        return '\n'.join(jsdata).decode('utf-8')
 
     def get_extensions(self, pagedir, extra_files):
         '''
@@ -77,6 +76,9 @@ class BEMRender(object):
 
         if ext is None:
             page_js = self.load_pagejs_data(pagedir, extra_files)
+            # represent Unicode symbols with JavaScript character escape sequences
+            # as workaround for PyV8 issue http://code.google.com/p/pyv8/issues/detail?id=152
+            page_js = js_escape_unicode(page_js)
             ext = PyV8.JSExtension(name, page_js)
             self.pageextensions[name] = ext
         return name, ext
@@ -102,7 +104,7 @@ class BEMRender(object):
         if self.cache_context:
             self.contexts[context_name] = context
 
-        return context, prepare.decode('utf8')
+        return context, prepare
 
 
     def render(self, pagedir, context, env, entrypoint,
@@ -133,3 +135,29 @@ class BEMRender(object):
                     return ctx.eval('JSON.stringify(bemjson)')
                 return ctx.eval('BEMHTML.apply(bemjson)')
 
+
+import re
+
+ESCAPABLE = re.compile(r'([^\x00-\x7f])')
+HAS_UTF8 = re.compile(r'[\x80-\xff]')
+
+
+def _js_escape_unicode_re_callack(match):
+    s = match.group(0)
+    n = ord(s)
+    if n < 0x10000:
+        return r'\u%04x' % (n,)
+    else:
+        # surrogate pair
+        n -= 0x10000
+        s1 = 0xd800 | ((n >> 10) & 0x3ff)
+        s2 = 0xdc00 | (n & 0x3ff)
+        return r'\u%04x\u%04x' % (s1, s2)
+
+def js_escape_unicode(s):
+    """Return an ASCII-only representation of a JavaScript string"""
+    if isinstance(s, str):
+        if HAS_UTF8.search(s) is None:
+            return s
+        s = s.decode('utf-8')
+    return str(ESCAPABLE.sub(_js_escape_unicode_re_callack, s))
